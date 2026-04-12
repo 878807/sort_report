624151bd189eb68646ee424f00ddf42de9eb7c13
878807 <cooper50607@gmail.com>
2026-04-13 00:06:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -120,6 +120,67 @@ async function insertionSort() {
     updateStats();
     await sleep(delay);
   }
+}function generateArray() {
+    const bars = document.getElementById("bars");
+    bars.innerHTML = "";
+
+    let arr = [];
+
+    for (let i = 0; i < 20; i++) {
+        let value = Math.floor(Math.random() * 100) + 10;
+        arr.push(value);
+
+        let bar = document.createElement("div");
+        bar.classList.add("bar");
+        bar.style.height = value + "px";
+
+        bars.appendChild(bar);
+    }
+
+    return arr;
+}
+
+let array = generateArray();
+
+function resetArray() {
+    array = generateArray();
+}
+
+async function startSort(type) {
+    if (type === "bubble") {
+        await bubbleSort();
+    }
+}
+
+async function bubbleSort() {
+    let bars = document.getElementsByClassName("bar");
+
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length - i - 1; j++) {
+
+            bars[j].style.background = "red";
+            bars[j + 1].style.background = "red";
+
+            await sleep(100);
+
+            if (array[j] > array[j + 1]) {
+                // swap
+                let temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+
+                bars[j].style.height = array[j] + "px";
+                bars[j + 1].style.height = array[j + 1] + "px";
+            }
+
+            bars[j].style.background = "steelblue";
+            bars[j + 1].style.background = "steelblue";
+        }
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 resetArray();
